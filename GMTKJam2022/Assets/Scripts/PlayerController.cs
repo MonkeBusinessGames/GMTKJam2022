16e90557ba6b8ae056b60f48e5c49df59022eccd
Ahnaf Hassan <absuloutezero@gmail.com>
2022-07-15 22:14:26 -0400

    Added Player Rotation
    Player now rotates towards mouse, and camera uses cinemachine

--- GMTKJam2022/Assets/Scripts/PlayerController.cs
+++ GMTKJam2022/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,14 @@ public class PlayerController : MonoBehaviour
     [Range(1f, 10.0f)]
     [SerializeField] private float speed = 5f;
 
-    private Rigidbody2D playerRb;
-
-    void Awake()
-    {
-        playerRb = GetComponent<Rigidbody2D>();
-    }
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private Camera cam;
 
     void FixedUpdate()
     {
         playerRb.velocity = new Vector2(getInput().x * speed, getInput().y * speed);
+        Vector2 lookDir = (Vector2) cam.ScreenToWorldPoint(Input.mousePosition) - playerRb.position;
+        playerRb.rotation = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
     }
     private Vector2 getInput()
     {
