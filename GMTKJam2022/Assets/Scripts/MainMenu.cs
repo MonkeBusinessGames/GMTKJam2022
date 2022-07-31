e6c07b3271d0a2939b1a7742e8cddce8608a4cb4
Ahnaf Hassan <absuloutezero@gmail.com>
2022-07-31 01:41:35 -0400

    Main Menu and Volume Updates
    Added a volume mixer and updated the main menu

--- GMTKJam2022/Assets/Scripts/MainMenu.cs
+++ GMTKJam2022/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject controls;
+    [SerializeField] GameObject credits;
+    [SerializeField] AudioMixer mixer;
+
+
     public void Play()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+    public void Menu()
+    {
+        controls.SetActive(false);
+        credits.SetActive(false);
+    }
+    public void Controls()
+    {
+        controls.SetActive(true);
+        credits.SetActive(false);
+    }
+    public void Credits()
+    {
+        controls.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void SetVolume(float value)
+    {
+        mixer.SetFloat("Volume", Mathf.Log(value) * 20);
     }
 }
